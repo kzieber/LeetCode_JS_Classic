0caf0da6c9b78b20d632a78d9b45d22821623f0e
Kevin Zieber <zieber.kevinr@gmail.com>
2021-06-23 06:57:09 -0700

    completed BFS traversal of a binary tree

--- exercises/LC102_levelOrder/index.js
+++ exercises/LC102_levelOrder/index.js
@@ -1,3 +1,25 @@
-function levelOrder(root) {}
+function levelOrder(root) {
+  const res = [];
+  const queue = [root];
+
+  if (root === null) return res;
+
+  while (queue.length > 0) {
+    const levelSize = queue.length;
+    const currentLevel = [];
+
+    for (let i = 0; i < levelSize; i++) {
+      let currentNode = queue.shift();
+      currentLevel.push(currentNode.val);
+
+      if (currentNode.left !== null) queue.push(currentNode.left);
+      if (currentNode.right !== null) queue.push(currentNode.right);
+    }
+
+    res.push(currentLevel);
+  }
+
+  return res;
+}
 
 module.exports = levelOrder;

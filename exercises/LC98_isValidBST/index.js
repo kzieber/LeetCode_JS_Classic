940626f6bc3909231626fb72a7bf0aed48accdb3
Kevin Zieber <zieber.kevinr@gmail.com>
2021-06-23 10:33:44 -0700

    validate BST completed

--- exercises/LC98_isValidBST/index.js
+++ exercises/LC98_isValidBST/index.js
@@ -1,3 +1,23 @@
-function isValidBST(root) {}
+function isValidBST(root) {
+  let valid = true;
+
+  function helper(node, min, max) {
+    if (!node) return;
+
+    if (
+      (min !== null && node.val <= min) ||
+      (max !== null && node.val >= max)
+    ) {
+      valid = false;
+      return;
+    }
+
+    helper(node.left, min, node.val);
+    helper(node.right, node.val, max);
+  }
+
+  helper(root, null, null);
+  return valid;
+}
 
 module.exports = isValidBST;
